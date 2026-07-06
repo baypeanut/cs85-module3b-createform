7e85328bc3e6f6184ebf3a5b50cfa81e67449300
Metin Yildiztas <aderici@unc.edu>
2026-07-05 21:56:45 -0400

    PHP submission handling logic

--- module3/ContactForm.php
+++ module3/ContactForm.php
@@ -1,9 +1,26 @@
+<?php
+// Metin Yildiztas
+// predictions: when i submit the form it should print back what i typed
+// i think $_POST will hold all field values like name email topic and message
+?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>Contact Form</title>
 </head>
 <body>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $topic = $_POST['topic'];
+    $message = $_POST['message'];
+
+    echo "<p>Thank you, " . $name . "! We received your message about: \"" . $topic . "\"</p>\n";
+    echo "<p>We'll get back to you at " . $email . ".</p>\n";
+}
+?>
 
 <form action="" method="POST">
     <p>Full Name: <input type="text" name="name" /></p>
