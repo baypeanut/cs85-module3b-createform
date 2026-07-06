61f4490a9ba310b35718e95387ae4c5901498afa
Metin Yildiztas <aderici@unc.edu>
2026-07-05 21:55:06 -0400

    Initial HTML form

---
+++ module3/ContactForm.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact Form</title>
+</head>
+<body>
+
+<form action="" method="POST">
+    <p>Full Name: <input type="text" name="name" /></p>
+    <p>Email Address: <input type="text" name="email" /></p>
+    <p>Topic of Message: <input type="text" name="topic" /></p>
+    <p>Message (50-150 words):<br />
+    <textarea name="message" rows="6" cols="50"></textarea></p>
+    <p><input type="submit" name="submit" value="Send" /></p>
+</form>
+
+</body>
+</html>
